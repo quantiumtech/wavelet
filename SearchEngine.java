3752b28cc346953b3b08d011ee653c64f9f4f3cb
Noah Kil <33174613+quantiumtech@users.noreply.github.com>
2022-10-06 13:48:37 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,36 @@
+// import java.io.IOException;
+// import java.net.URI;
+
+
+// class Handler implements URLHandler {
+//     // The one bit of state on the server: a number that will be manipulated by
+//     // various requests.
+
+//     public String handleRequest(URI url) {
+//             String[] inputs;
+//             int index = 0;
+                    
+//                     if (url.getPath().contains("/add")) {
+//                         String[] parameters = url.getQuery().split("=");
+//                         if (parameters[0].equals("s")) {
+//                             inputs[index].add("test";)
+//                      }
+//                     }
+//                    }
+    
+
+
+// public class SearchEngine {
+
+//         public static void main(String[] args) throws IOException {
+//             if(args.length == 0){
+//                 System.out.println("Missing port number! Try any number between 1024 to 49151");
+//                 return;
+//             }
+    
+//             int port = Integer.parseInt(args[0]);
+    
+//             Server.start(port, new Handler());
+//         }
+    
+// }
