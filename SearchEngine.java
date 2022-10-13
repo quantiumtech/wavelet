f03b1cd0b17f5a638d280f59450a015946bdb0f2
Noah Kil <33174613+quantiumtech@users.noreply.github.com>
2022-10-13 01:30:21 -0700

    Fully implemented basic search engine

--- SearchEngine.java
+++ SearchEngine.java
@@ -5,10 +5,8 @@ import java.util.ArrayList;
 class SearchHandler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
-
+    ArrayList<String> words = new ArrayList<String>();
     public String handleRequest(URI url) {
-        ArrayList<String> words = new ArrayList<String>();
         if (url.getPath().equals("/")) {
             return "Welcome to my search engine! Use add?s= to add a new word and search?s= to search for all added words.";
         }
@@ -17,21 +15,24 @@ class SearchHandler implements URLHandler {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
                     words.add(parameters[1]);
-                    return parameters[1].concat(" added.");
+                    return parameters[1].concat(" added. Elements in server: " + words.size());
                 }
             }
-            else if (url.getPath().contains("/search")) {
+             if (url.getPath().contains("/search")) {
                 String[] parameters = url.getQuery().split("=");
                 String anOutput = "Your search result is: ";
+                int strLen = anOutput.length();
                 if (parameters[0].equals("s")) {
                     String search = parameters[1];
                     for (int i = 0; i < words.size(); i++) {
                             if (words.get(i).indexOf(search) > -1) {
-                                anOutput.concat(words.get(i));
-                                if ((i + 1) < words.size()) {
-                                    anOutput.concat(" and ");
-                                }
+                                anOutput += words.get(i);
+                                anOutput = anOutput.concat(" and ");
                             } 
+
+                    }
+                    if (anOutput.length() > strLen) {
+                        anOutput = anOutput.substring(0, anOutput.length()-4);
                     }
                     return anOutput;
                 }
