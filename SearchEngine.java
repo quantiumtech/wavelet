f0b464e1c0797249ab9d7ccec6a85c8cf68947b8
Noah Kil <33174613+quantiumtech@users.noreply.github.com>
2022-10-13 00:47:15 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,47 +1,58 @@
-// import java.io.IOException;
-// import java.net.URI;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
 
-//// pseudocode: {
-    ////follow same steps as NumberServer in order to get the right input
-    ////store user input in separate array, interate through using loop when called w/ search
-    ////use TBD java function in order to compare strings to substrings
+class SearchHandler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
 
-////}
-
-
-
-
-
-
-// class Handler implements URLHandler {
-//     // The one bit of state on the server: a number that will be manipulated by
-//     // various requests.
-
-//     public String handleRequest(URI url) {
-//             String[] inputs;
-//             int index = 0;
-                    
-//                     if (url.getPath().contains("/add")) {
-//                         String[] parameters = url.getQuery().split("=");
-//                         if (parameters[0].equals("s")) {
-//                             inputs[index].add("test";)
-//                      }
-//                     }
-//                    }
+    public String handleRequest(URI url) {
+        ArrayList<String> words = new ArrayList<String>();
+        if (url.getPath().equals("/")) {
+            return "Welcome to my search engine! Use add?s= to add a new word and search?s= to search for all added words.";
+        }
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    words.add(parameters[1]);
+                    return parameters[1].concat(" added.");
+                }
+            }
+            else if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                String anOutput = "Your search result is: ";
+                if (parameters[0].equals("s")) {
+                    String search = parameters[1];
+                    for (int i = 0; i < words.size(); i++) {
+                            if (words.get(i).indexOf(search) > -1) {
+                                anOutput.concat(words.get(i));
+                                if ((i + 1) < words.size()) {
+                                    anOutput.concat(" and ");
+                                }
+                            } 
+                    }
+                    return anOutput;
+                }
+            }
+            return "404 Not Found!";
     
+        }
+    }
 
+class SearchEngine {
 
-// public class SearchEngine {
+        public static void main(String[] args) throws IOException {
+            if(args.length == 0){
+                System.out.println("Missing port number! Try any number between 1024 to 49151");
+                return;
+            }
+    
+            int port = Integer.parseInt(args[0]);
+    
+            Server.start(port, new SearchHandler());
+        }
+    
+}
 
-//         public static void main(String[] args) throws IOException {
-//             if(args.length == 0){
-//                 System.out.println("Missing port number! Try any number between 1024 to 49151");
-//                 return;
-//             }
-    
-//             int port = Integer.parseInt(args[0]);
-    
-//             Server.start(port, new Handler());
-//         }
-    
-// }
